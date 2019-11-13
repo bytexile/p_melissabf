0768550ca2f0a82a8c3ef4d325f9833c398eaaa3
jonas <sanoj.camargo@mail.com>
2019-11-13 16:01:37 -0300

    tela teste de envio de email

--- app/Controller/site/PageController.php
+++ app/Controller/site/PageController.php
@@ -335,8 +335,56 @@ class PageController extends BaseController
 
 
     // ---------- VIEW - AGRADECIMENTO ---------------------
-    public function sendEmail()
+    public function sendEmail($params = null)
     {
-        echo '<form action="/teste-email"><input type="email" name="email" placeholder="E-mail para enviar"><input type="submit" value="enviar"></form>';
+        if( !$params ){
+            echo '<form action="/teste-email" method="GET"><input type="email" name="email" placeholder="E-mail para enviar"><input type="submit" value="enviar"></form>';
+        }
+        else {
+            echo'<pre>';
+            print_r($params->post->email);
+            echo'</pre>';
+
+
+            //Variaveis de POST, Alterar somente se necessário
+            //====================================================
+            $nome  = 'jonas';
+            $email = $params->post->email;
+            //====================================================
+
+            //REMETENTE --> ESTE EMAIL TEM QUE SER VALIDO DO DOMINIO
+            //====================================================
+            $email_remetente = $email; // deve ser uma conta de email do seu dominio
+            //====================================================
+
+            //Configurações do email, ajustar conforme necessidade
+            //====================================================
+            $email_destinatario = $email; // pode ser qualquer email que receberá as mensagens
+            $email_reply        = "$email";
+            $email_assunto      = "Novo contato de " . $nome . ", pelo site!"; // Este será o assunto da mensagem
+            //====================================================
+
+            //Monta o Corpo da Mensagem
+            //====================================================
+            $email_conteudo  = "Nome: $nome \n";
+            $email_conteudo .= "Email: $email \n";
+            //====================================================
+
+            //Seta os Headers (Alterar somente caso necessario)
+            //====================================================
+            $email_headers = implode ( "\n",array ( "From: $email_remetente", "Reply-To: $email_reply", "Return-Path: $email_remetente","MIME-Version: 1.0","X-Priority: 3","Content-Type: text/html; charset=UTF-8" ) );
+            //====================================================
+
+
+            //Enviando o email
+            //====================================================
+            if ( @mail($email_destinatario, $email_assunto, nl2br($email_conteudo), $email_headers) ){
+                echo '<h1>Email enviado com sucesso!</h1>';
+            }
+            else {
+                echo '<h1>Ocorreu um erro...</h1>';
+            }
+            //====================================================
+        }
     }
 }
