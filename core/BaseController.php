bf1a3e45638edffb93b2e73a913b2a694dd84f02
jonas <sanoj.camargo@mail.com>
2019-11-14 15:56:53 -0300

    captura de IP para cadastro de location

--- core/BaseController.php
+++ core/BaseController.php
@@ -85,6 +85,26 @@ abstract class BaseController
 
     public function meuIP()
     {
-        echo var_export(unserialize(file_get_contents('http://www.geoplugin.net/php.gp?ip='.$_SERVER['REMOTE_ADDR'])));
+        function get_client_ip_env() {
+            $ipaddress = '';
+            if (getenv('HTTP_CLIENT_IP'))
+                $ipaddress = getenv('HTTP_CLIENT_IP');
+            else if(getenv('HTTP_X_FORWARDED_FOR'))
+                $ipaddress = getenv('HTTP_X_FORWARDED_FOR');
+            else if(getenv('HTTP_X_FORWARDED'))
+                $ipaddress = getenv('HTTP_X_FORWARDED');
+            else if(getenv('HTTP_FORWARDED_FOR'))
+                $ipaddress = getenv('HTTP_FORWARDED_FOR');
+            else if(getenv('HTTP_FORWARDED'))
+                $ipaddress = getenv('HTTP_FORWARDED');
+            else if(getenv('REMOTE_ADDR'))
+                $ipaddress = getenv('REMOTE_ADDR');
+            else
+                $ipaddress = 'UNKNOWN';
+        
+            return $ipaddress;
+        }
+
+        echo var_export(unserialize(file_get_contents('http://www.geoplugin.net/php.gp?ip='.get_client_ip_env())));
     }
 }
