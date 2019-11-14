0599c65925de8b077297a2cce022f663ab49ecbc
jonas <sanoj.camargo@mail.com>
2019-11-14 15:51:40 -0300

    pegar ip do usuario

--- core/BaseController.php
+++ core/BaseController.php
@@ -85,6 +85,24 @@ abstract class BaseController
 
     public function meuIP()
     {
-        echo var_export(unserialize(file_get_contents('http://www.geoplugin.net/php.gp?ip='.$_SERVER['REMOTE_ADDR'])));
+        $ipaddress = '';
+
+        if ($_SERVER['HTTP_CLIENT_IP'])
+            $ipaddress = $_SERVER['HTTP_CLIENT_IP'];
+        else if($_SERVER['HTTP_X_FORWARDED_FOR'])
+            $ipaddress = $_SERVER['HTTP_X_FORWARDED_FOR'];
+        else if($_SERVER['HTTP_X_FORWARDED'])
+            $ipaddress = $_SERVER['HTTP_X_FORWARDED'];
+        else if($_SERVER['HTTP_FORWARDED_FOR'])
+            $ipaddress = $_SERVER['HTTP_FORWARDED_FOR'];
+        else if($_SERVER['HTTP_FORWARDED'])
+            $ipaddress = $_SERVER['HTTP_FORWARDED'];
+        else if($_SERVER['REMOTE_ADDR'])
+            $ipaddress = $_SERVER['REMOTE_ADDR'];
+        else
+            $ipaddress = 'UNKNOWN';
+            
+
+        echo var_export(unserialize(file_get_contents('http://www.geoplugin.net/php.gp?ip='.$ipaddress)));
     }
 }
