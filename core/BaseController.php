0aa88b93fa8d5a22e4e0d5ba776d5333928aadf9
jonas <sanoj.camargo@mail.com>
2019-11-14 15:30:06 -0300

    captura do ip para o location

--- core/BaseController.php
+++ core/BaseController.php
@@ -86,21 +86,16 @@ abstract class BaseController
     // --------- APAGAR
     public function meuIP()
     {
-        //whether ip is from share internet
-if (!empty($_SERVER['HTTP_CLIENT_IP']))   
-{
-  $ip_address = $_SERVER['HTTP_CLIENT_IP'];
-}
-//whether ip is from proxy
-elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR']))  
-{
-  $ip_address = $_SERVER['HTTP_X_FORWARDED_FOR'];
-}
-//whether ip is from remote address
-else
-{
-  $ip_address = $_SERVER['REMOTE_ADDR'];
-}
-echo $ip_address;
+        if (!empty($_SERVER['HTTP_CLIENT_IP'])){
+            $ip_address = $_SERVER['HTTP_CLIENT_IP'];
+        }
+        else if (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])){
+            $ip_address = $_SERVER['HTTP_X_FORWARDED_FOR'];
+        }
+        else {
+            $ip_address = $_SERVER['REMOTE_ADDR'];
+        }
+        
+        echo $ip_address;
     }
 }
