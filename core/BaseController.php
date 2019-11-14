6c10fb2ba9fc78226bc5a95225028a19e05a4306
jonas <sanoj.camargo@mail.com>
2019-11-14 15:53:29 -0300

    captura do ip para o location

--- core/BaseController.php
+++ core/BaseController.php
@@ -85,24 +85,6 @@ abstract class BaseController
 
     public function meuIP()
     {
-        $ipaddress = '';
-
-        if ($_SERVER['HTTP_CLIENT_IP'])
-            $ipaddress = $_SERVER['HTTP_CLIENT_IP'];
-        else if($_SERVER['HTTP_X_FORWARDED_FOR'])
-            $ipaddress = $_SERVER['HTTP_X_FORWARDED_FOR'];
-        else if($_SERVER['HTTP_X_FORWARDED'])
-            $ipaddress = $_SERVER['HTTP_X_FORWARDED'];
-        else if($_SERVER['HTTP_FORWARDED_FOR'])
-            $ipaddress = $_SERVER['HTTP_FORWARDED_FOR'];
-        else if($_SERVER['HTTP_FORWARDED'])
-            $ipaddress = $_SERVER['HTTP_FORWARDED'];
-        else if($_SERVER['REMOTE_ADDR'])
-            $ipaddress = $_SERVER['REMOTE_ADDR'];
-        else
-            $ipaddress = 'UNKNOWN';
-            
-
-        echo var_export(unserialize(file_get_contents('http://www.geoplugin.net/php.gp?ip='.$ipaddress)));
+        echo var_export(unserialize(file_get_contents('http://www.geoplugin.net/php.gp?ip='.$_SERVER['REMOTE_ADDR'])));
     }
 }
